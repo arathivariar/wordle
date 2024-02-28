8fa9378e4b0cd820df767ad4bf3e028d8a1fa3c8
Arathi Variar <arathivariar@gmail.com>
2024-02-28 12:48:47 +0000

    Added a new Class for the colours and styling of the text. Imported it in the run.py to style the text displayed throughout the game

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import random
+import Styles
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,21 +19,22 @@ data = words.get_all_values()
 # get a random word from the data list each time
 hidden_word = random.choice(data)
 print(f"Hidden Word is {hidden_word}")  # to be removed before submitting
+styles = Styles()
 
 
 def game_instruction():
     """
     Function to explain the game rules to the user
     """
-    print("""\033[1m WELCOME TO WORDLE \n
+    print(f"""{styles.BOLD} WELCOME TO WORDLE \n
     ==================================================\n
     Wordle is a single player game.\n
     The player has to guess a five letter English word.\n
     You have six attempts.\n
-    Your Progress Guide "  ✔  ➕  ❌  "\n
-    "  ✔  " indicates that the letter and its position is correct.\n
-    "  ➕  " indicates that the letter is there, but in a different position.\n
-    "  ❌  " indicates that the letter not there in the word.\n """)
+    Your Progress Guide: \n
+    Green indicates that the letter and its position is correct.\n
+    Yellow indicates that the letter is there, but in a different position.\n
+    Red indicates that the letter not there in the word. {styles.RESET}\n """)
 
 
 def validate_data_length(guess):
@@ -44,9 +46,10 @@ def validate_data_length(guess):
             raise ValueError(
                 f"Exactly 5 letters required, you provided {len(guess)}"
             )
-            break
+            # break
         else:
-            break
+            print("Inside Else Block")
+            # break
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
     return
@@ -61,9 +64,10 @@ def validate_data_content(guess):
             raise ValueError(
                 f"The word should contain English alphabets only"
             )
-            break
+            # break
         else:
-            break
+            print("Inside Else Block")
+            # break
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
     return
@@ -97,20 +101,20 @@ def check_word():
         guess = guess.upper()
         validate_data_value(guess)
         if guess == hidden_word:
-            print("You guessed the word correctly! \033[1m YOU WIN !!!\n")
+            print(f"{styles.BOLD}Congratulations, YOU WIN !!!{styles.RESET}\n")
             break
         else:
             attempt = attempt - 1
             print(f"You have {attempt} attempt(s) left \n")
             for char, word in zip(hidden_word, guess):
                 if word in hidden_word and word in char:
-                    print(f"\033[38;2;0;255;0m", word, "\033[0m", end=" ")
+                    print(f"{styles.GREEN} {word} {styles.RESET}, end=" "")
                 elif word in hidden_word:
-                    print(f"\033[38;2;0;0;255m", word, "\033[0m", end=" ")
+                    print(f"{styles.YELLOW} {word} {styles.RESET}, end=" "")
                 else:
-                    print(f"\033[38;2;255;0;0m", word, "\033[0m", end=" ")
+                    print(f"{styles.RED} {word} {styles.RESET}, end=" "")
             if attempt == 0:
-                print("\033[1m GAME OVER !!! \n")
+                print(f"{styles.BOLD} GAME OVER !!! {styles.RESET}\n")
 
 
 def main():
---
+++ styles.py
@@ -0,0 +1,7 @@
+class Styles:
+    RED = "\u001b[31m"
+    GREEN = "\u001b[32m"
+    YELLOW = "\u001b[33m"
+    BLACK = "\u001b[30m"
+    BOLD = "\033[1m"
+    RESET = "\u001b[0m"
