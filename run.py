e0ffb6ab947fc784a1cc881eb025b89c67ab46e4
Arathi Variar <arathivariar@gmail.com>
2024-02-28 14:24:37 +0000

    Fixed the error in the display messages by replacing double quoutes with single quotes for the end parameter

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import random
-import Styles
+from styles import Styles
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,14 +19,14 @@ data = words.get_all_values()
 # get a random word from the word list each time
 hidden_word = random.choice(data)
 print(f"Hidden Word is {hidden_word}")  # to be removed before submitting
-styles = Styles()
+# Styles = Styles()
 
 
 def game_instruction():
     """
     Function to explain the game rules to the user
     """
-    print(f"""{styles.BOLD} WELCOME TO WORDLE \n
+    print(f"""{Styles.BOLD} WELCOME TO WORDLE \n
     ==================================================\n
     Wordle is a single player game.\n
     The player has to guess a five letter English word.\n
@@ -34,7 +34,7 @@ def game_instruction():
     Your Progress Guide: \n
     Green indicates that the letter and its position is correct.\n
     Yellow indicates that the letter is there, but in a different position.\n
-    Red indicates that the letter not there in the word. {styles.RESET}\n """)
+    Red indicates that the letter not there in the word. {Styles.RESET}\n """)
 
 
 def validate_data_length(guess):
@@ -101,20 +101,20 @@ def check_word():
         guess = guess.upper()
         validate_data_value(guess)
         if guess == hidden_word:
-            print(f"{styles.BOLD}Congratulations, YOU WIN !!!{styles.RESET}\n")
+            print(f"{Styles.BOLD}Congratulations, YOU WIN !!!{Styles.RESET}\n")
             break
         else:
             attempt = attempt - 1
             print(f"You have {attempt} attempt(s) left \n")
             for char, word in zip(hidden_word, guess):
                 if word in hidden_word and word in char:
-                    print(f"{styles.GREEN} {word} {styles.RESET}, end=" "")
+                    print(f"{Styles.GREEN} {word} {Styles.RESET}", end =' ')
                 elif word in hidden_word:
-                    print(f"{styles.YELLOW} {word} {styles.RESET}, end=" "")
+                    print(f"{Styles.YELLOW} {word} {Styles.RESET}", end =' ')
                 else:
-                    print(f"{styles.RED} {word} {styles.RESET}, end=" "")
+                    print(f"{Styles.RED} {word} {Styles.RESET}", end =' ')
             if attempt == 0:
-                print(f"{styles.BOLD} GAME OVER !!! {styles.RESET}\n")
+                print(f"{Styles.BOLD} GAME OVER !!! {Styles.RESET}\n")
 
 
 def main():
